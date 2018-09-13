67395579ebffd82e768b6938b0c5f39e2181acff
Akivamu <akivamu@gmail.com>
2018-09-13 15:16:44 +0700

    users

--- ert-db/init.sql
+++ ert-db/init.sql
@@ -9,7 +9,11 @@ CREATE TABLE IF NOT EXISTS users (
   PRIMARY KEY (id)
 );
 
-INSERT INTO users (username, password, full_name, dob) VALUES ("admin", "admin", "Administrator", "1988-11-11");
+INSERT INTO users (username, password, full_name, dob) VALUES ("alice", "123", "Liz Alice", "1988-11-11");
+INSERT INTO users (username, password, full_name, dob) VALUES ("bob", "123", "Sean Bob", "1990-01-21");
+INSERT INTO users (username, password, full_name, dob) VALUES ("charlie", "123", "Carter Charlie", "1980-02-22");
+INSERT INTO users (username, password, full_name, dob) VALUES ("david", "123", "Kyron David", "1992-05-01");
+INSERT INTO users (username, password, full_name, dob) VALUES ("frank", "123", "Frank Shannon", "1950-09-09");
 
 CREATE TABLE IF NOT EXISTS oauth_clients (
   id INT NOT NULL AUTO_INCREMENT,
